91c600b5564fcfc4ff921aa730edda84cbee4099
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2026-08-08 09:44:23 +0530

    Time: 2 ms (92.07%), Space: 47.8 MB (57.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,11 +3,9 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                if(i!=j){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
             }
         }
